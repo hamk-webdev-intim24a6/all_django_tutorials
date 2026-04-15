76096f97eb81c42c1b0d9408f60febd2ba147e51
petrikuittinen <petri.kuittinen@hamk.fi>
2026-04-15 19:19:40 +0300

    gallery tutorial completed

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'gallery.apps.GalleryConfig',    
     'guestbook.apps.GuestbookConfig',
     'account.apps.AccountConfig',
     'dictionary.apps.DictionaryConfig',
@@ -123,3 +125,5 @@ LOGIN_REQUIRED = 'login'
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
