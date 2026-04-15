b9f0226d1e67f4d18473289cca84ff94d6362d77
petrikuittinen <petri.kuittinen@hamk.fi>
2026-04-15 18:34:55 +0300

    guestbook tutorial completed

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -25,8 +25,8 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', SignUpView.as_view(), name='signup'),
-
     path('admin/', admin.site.urls),
     path('hello/', include('hello.urls')),
     path('dictionary/', include('dictionary.urls')),
+    path('guestbook/', include('guestbook.urls')),
 ]
